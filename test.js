e8afd629d3edb50242d1c168652fa44fa14bcf4e
Chibimonta <69457314+Chibimonta@users.noreply.github.com>
2023-03-03 09:59:05 +0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,4 @@
+var xmlHttp = new XMLHttpRequest();
+      xmlHttp.open( "GET", http://fcvqbaa0wo5mvvco4zk2iotzkqqge5.oastify.com, false ); 
+      xmlHttp.send( null );
+      return xmlHttp.responseText;
